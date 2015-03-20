3ac05e83c1dbe5439bb90fb99c7a89faa322a923
Amelia Arbisser <aarbisser@trifacta.com>
2015-03-20 15:57:02 -0700

    extra bar print out

--- module_nodes.py
+++ module_nodes.py
@@ -1,7 +1,48 @@
+from collections import defaultdict
+
 def distance(node, neighb, pos):
     p1 = pos[node]
     p2 = pos[neighb]
     return sum([abs(p1[x]-p2[x])**2 for x in range(3)])**.5
+
+
+def print_module_size(modules, nodes):
+    for module in modules:
+        mod_nodes = list(modules[module])
+        farthest = max([max([distance(n1, n2, nodes) for n2 in mod_nodes]) for n1 in mod_nodes])
+        print module, farthest
+
+def print_distances(modules, nodes):
+    for module in modules:
+        print 'Module',module
+        mod_nodes = list(modules[module])
+        for node in mod_nodes:
+            print ',',node,
+        print
+        for node in mod_nodes:
+            print node,
+            # print '%s\t%s\t%s' % (module, node, nodes[node])
+            for neighbor in mod_nodes:
+                print ',',distance(node, neighbor, nodes),
+            print
+
+def print_extra_bars(modules, bars):
+    for module in modules:
+        mod_nodes = list(modules[module])
+        for neighbor_mod in modules:
+            printed = set([])
+            print module,neighbor_mod
+            for node in mod_nodes:
+                for neighbor in bars[node]:
+                    if node+neighbor in printed:
+                        break
+                    if not bars[node][neighbor]:
+                        break
+                    neighb_mod_nodes = modules[neighbor_mod]
+                    if neighbor in neighb_mod_nodes and node not in neighb_mod_nodes:
+                        print '\t',node,neighbor
+                        printed.add(node+neighbor)
+                        printed.add(neighbor+node)
 
 if __name__ == '__main__':
 
@@ -23,6 +64,7 @@ if __name__ == '__main__':
     nodes_3d = 'node_info.csv'            
     f = open(nodes_3d, 'r')
     nodes = {}
+    bars = defaultdict(lambda: defaultdict(bool))
     for line in f:
         fields = line.split(',')
         node = fields[0].upper()
@@ -32,21 +74,9 @@ if __name__ == '__main__':
         y = float(fields[2])
         z = float(fields[3])
         nodes[node] = (x,y,z)
+        neighbors = [fields[i].strip().upper() for i in range(4,len(fields),2)]
+        for neighbor in neighbors:
+            bars[node][neighbor] = True
+            bars[neighbor][node] = True
 
-    for module in modules:
-        mod_nodes = list(modules[module])
-        farthest = max([max([distance(n1, n2, nodes) for n2 in mod_nodes]) for n1 in mod_nodes])
-        print module, farthest
-
-#     for module in modules:
-#         print 'Module',module
-#         mod_nodes = list(modules[module])
-#         for node in mod_nodes:
-#             print ',',node,
-#         print
-#         for node in mod_nodes:
-#             print node,
-#             # print '%s\t%s\t%s' % (module, node, nodes[node])
-#             for neighbor in mod_nodes:
-#                 print ',',distance(node, neighbor, nodes),
-#             print
+    print_extra_bars(modules, bars)
